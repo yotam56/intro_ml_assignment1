9c6b061a8e0f9ae129dda5e0d198b2f8f30f3dbf
Yotam56 <yotam56@gmail.com>
2024-01-19 15:32:33 +0200

    q_2_e

--- nearest_neighbour.py
+++ nearest_neighbour.py
@@ -39,6 +39,7 @@ def euclidean_distance(x1, x2):
     """
     return np.linalg.norm(x1 - x2)
 
+
 def learnknn(k: int, x_train: np.array, y_train: np.array):
     """
 
@@ -49,6 +50,7 @@ def learnknn(k: int, x_train: np.array, y_train: np.array):
     """
     classifier = {'k': k, 'x_train': x_train, 'y_train': y_train}
     return classifier
+
 
 def predictknn(classifier, x_test: np.array):
     """
@@ -73,7 +75,6 @@ def predictknn(classifier, x_test: np.array):
 
 
 def simple_test():
-
     train0 = data['train0']
     train1 = data['train1']
     train2 = data['train2']
@@ -105,7 +106,6 @@ def simple_test():
 
 
 def test_knn():
-
     x_train = np.array([[1, 2], [2, 3], [3, 4], [4, 5], [5, 6]])
     y_train = np.array([0, 1, 0, 1, 0])
     x_test = np.array([[1, 1], [4, 4], [6, 6]])
@@ -160,11 +160,50 @@ def question_2a():
     plt.title('KNN Test Error vs Training Sample Size')
     plt.errorbar(x, y, yerr=yerr, capsize=3, fmt="r--o", ecolor="black")
     plt.show()
+
+
+def question_2e():
+    # data = np.load("mnist_all.npz")
+    average_errors = []
+    error_min = []
+    error_max = []
+    k_nums = [k for k in range(1, 12)]
+    for k in k_nums:
+        errors = []
+        for _ in range(10):
+            x_train, y_train = gensmallm([data["train2"], data["train3"], data["train5"], data["train6"]],
+                                         [2, 3, 5, 6], 200)
+            x_test, y_test = gensmallm([data["test2"], data["test3"], data["test5"], data["test6"]],
+                                       [2, 3, 5, 6], 200)
+
+            classifier = learnknn(k, x_train, y_train)
+            y_test_pred = predictknn(classifier, x_test)
+
+            error = np.mean(y_test != list(np.concatenate(y_test_pred).flat))
+            errors.append(error)
+
+        average_errors.append(np.mean(errors))
+        error_min.append(np.min(errors))
+        error_max.append(np.max(errors))
+
+    plt.scatter(k_nums, error_max, color="green")
+    plt.scatter(k_nums, error_min, color="red")
+    plt.scatter(k_nums, average_errors, color="blue")
+    x = k_nums
+    y = average_errors
+    up = [item1 - item2 for item1, item2 in zip(error_max, average_errors)]
+    down = [item1 - item2 for item1, item2 in zip(average_errors, error_min)]
+    yerr = [down, up]
+    plt.xlabel('K size')
+    plt.ylabel('Average Test Error')
+    plt.title('KNN Test Error vs K Size')
+    plt.errorbar(x, y, yerr=yerr, capsize=3, fmt="r--o", ecolor="black")
+    plt.show()
+
+
 if __name__ == '__main__':
-
     # before submitting, make sure that the function simple_test runs without errors
-    question_2a()
-    # test_knn()
     # simple_test()
-
-
+    # test_knn()
+    # question_2a()
+    question_2e()
